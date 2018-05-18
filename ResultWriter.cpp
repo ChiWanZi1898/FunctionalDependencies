44477aecfd3851ea22c9662cd0e5ed6f84054d37
Hongyu Zhu <aglax@MBP.local>
2018-05-18 17:56:33 +0800

    feat: add output part

---
+++ ResultWriter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Hongyu Zhu on 2018/5/18.
+//
+
+#include "ResultWriter.h"
---
+++ ResultWriter.h
@@ -0,0 +1,14 @@
+//
+// Created by Hongyu Zhu on 2018/5/18.
+//
+
+#ifndef FD_RESULTWRITER_H
+#define FD_RESULTWRITER_H
+
+
+class ResultWriter {
+
+};
+
+
+#endif //FD_RESULTWRITER_H
